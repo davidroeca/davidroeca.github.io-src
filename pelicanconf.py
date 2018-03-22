2c35e993653aef4943ccdea7e81a8bf2b596120f
davidroeca <david.roeca@gmail.com>
2018-03-22 14:00:45 -0400

    update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import datetime
 
+# Theme
+THEME = "themes/MinimalXY"
+MINIMALXY_START_YEAR = 2018
+MINIMALXY_CURRENT_YEAR = datetime.now().year
+
+# Author
 AUTHOR = 'David Roeca'
+
 SITENAME = "David's Personal Site"
 SITEURL = ''
-
 PATH = 'content'
 
 TIMEZONE = 'EST'
-
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
@@ -27,7 +33,7 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ('My Github', 'https://github.com/davidroeca'),
+    ('github', 'https://github.com/davidroeca'),
 )
 
 DEFAULT_PAGINATION = 10
