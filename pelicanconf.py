818452f5eadca9a929493dbfae380be6b9701c64
davidroeca <david.roeca@gmail.com>
2018-03-22 13:42:16 -0400

    change configs

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,14 +20,15 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('My Github', 'https://github.com/davidroeca'),
+)
 
 DEFAULT_PAGINATION = 10
 
